0bb9278f3c01589233935c68dd45a23a96791354
maninath <30830395+maninath@users.noreply.github.com>
2017-08-08 16:24:15 +0530

    4.c

---
+++ 4./4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+char input[30]; 
+int wordlen;
+
+printf("please enter a word: \n");
+scanf("%29c", input);
+
+while (input < 30);
+{
+/*Not sure what to put in here*/
+printf("Number of letters in input is %s", /*???*/);
+}
+
+return 0;
+}
